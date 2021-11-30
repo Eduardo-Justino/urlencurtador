69599a3dfeab521faf290764f1b862c8d6887641
eduardo <justinoeduardo657@gmail.com>
2021-11-30 14:13:03 -0300

    projeto ok

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: 
+    'mongodb+srv://justinoeduardo657:dio@urlshortener-dio.go52y.mongodb.net/urlshortener?retryWrites=true&w=majority'
+}
